bb063879bbc94844f31e5cdb64c45713525e602e
Michael Daniel Murillo López <michaeldanielm@icloud.com>
2018-04-08 16:19:10 -0500

    Corporación Universitaria Minuto de Dios - Uniminuto

---
+++ Taller 2/Ejercicio 1.php
@@ -0,0 +1,18 @@
+<style>
+table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+}
+th, td {
+    padding: 9px;
+    text-align: left;    
+}
+</style>
+    <?php 
+for($i=1;$i<=100;$i++) 
+    echo " <center>
+    <table style='width:5%'><tr>
+            <td>$i</td>
+        </tr>
+    </table></center>"; 
+?>
---
+++ Taller 2/Ejercicio 4.php
@@ -0,0 +1,6 @@
+<?php
+$suma=0;
+for($i=1;$i<=100;$i++) 
+$suma=$suma+$i;    
+echo"<h2>La suma de los 100 primeros numeros es:<h2>".$suma;
+?>
