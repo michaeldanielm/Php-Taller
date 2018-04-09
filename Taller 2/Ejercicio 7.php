2e55156c568c8c50c9a33926a1111e3fc5308ec4
Michael Daniel Murillo López <michaeldanielm@icloud.com>
2018-04-09 18:51:01 -0500

    Update Ejercicio 7.php

--- Taller 2/Ejercicio 7.php
+++ Taller 2/Ejercicio 7.php
@@ -1,19 +1,31 @@
 <style>
-table, th, td {
-    border: 1px solid black;
-    border-collapse: collapse;
-}
-th, td {
-    padding: 9px;
-    text-align: left;    
-}
-</style>
-    <?php 
+    table,
+    th,
+    td {
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
 
-for($i=3;$i<=100;$i=$i+3) 
+    th,
+    td {
+        padding: 9px;
+        text-align: left;
+    }
+    
+
+</style>
+<form action="" method="post">
+<input type="number"  name="s">
+<input type="submit" value="Ejecutar" name="Ejecutar">
+</form>
+<?php 
+if(isset($_POST['Ejecutar'])){
+	$s=$_POST['s'];
+	for($i=3;$i<=$s;$i=$i+3) 
     echo " <center>
     <table style='width:5%'><tr>
             <td>$i</td>
         </tr>
-    </table></center>"; 
+    </table></center>";
+}	
 ?>
